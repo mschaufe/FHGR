3d15e80497c36df6fa4b66e7d308fe655ce0ddcc
Marc Schaufelberger <marc@schufi.ch>
2018-10-18 10:45:56 +0200

    add matlab folder and exercises

--- matlab/Anal1Serie5/TermeAuswerten.m
+++ matlab/Anal1Serie5/TermeAuswerten.m
@@ -1,10 +1,38 @@
 % MATLAB initialisieren:
 clear all; clc; format compact; format short g; 
 
-% Parameter:
-
-%Funktionen:
-
-%Daten:
-
-%Plot:
+% Aufgabe 2
+    % Potenz
+    a2=(45.6)^3
+    % Quadrat-Wurzel
+    b2=sqrt(3)
+    % Wurzel
+    c2=2.^(1/5);
+    % Wurzel und Potenz
+    d2=(5.654*(10^24))^(1/7)
+    d21=5.654e24^(1/7)
+    % Wurzel und Potenz
+    e2=44.5^(5/9)
+    % PI
+    f2=4*pi
+    % sin()
+    g2=sin(5*pi/4)
+    % co tan()
+    h2=cot(-3*pi/5)
+    % tan()
+    i2=tand(77)
+    % arccos()
+    j2=acosd(-0.45)
+    % arccot()
+    k2=acotd(34.1)
+    % Eulersche Zahl
+    l2=exp(-3.34)
+    % Eulersche Zahl
+    m2= exp(1)
+    % Log
+    n2= log(13.2)
+    % Log 10
+    o2= log(23456)/log(10)
+    % Log 2
+    p2= log(69.6)/log(2)
+    
