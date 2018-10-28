757cc85baf607aa40835c175c1ee0f980e4b3b14
mschaufe <marc@schufi.ch>
2018-10-28 08:46:30 +0100

    Add KomplexeZahlen

---
+++ matlab/Linalg/KomplexeZahlen.m
@@ -0,0 +1,20 @@
+% MATLAB initialisieren:
+clear all; clc; format compact; format short g; 
+
+%Komplexe Zahlen
+    
+    x = 5;
+    y = 31;
+    r=2;
+
+    % Betrag
+    KB = r*exp(1)^(i*2*pi);
+    ohne_betrag = KB
+    mit_betrag = abs(KB)
+
+    % Konjugation
+    KZ=(x + y*i);
+    ohne_konjugation = KZ
+    mit_konjugation = conj(KZ)
+    
+    
---
+++ matlab_function/cis.m
@@ -0,0 +1,6 @@
+function [outputArg1] = cis(inputArg1)
+% cis function
+
+%   Ausrechnung der trigometrischen Summe
+outputArg1 = cos(inputArg1) + i*sin(inputArg1);
+end
