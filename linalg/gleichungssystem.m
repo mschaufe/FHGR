593c556942706b85beee845015dc4a4391cb26a0
mschaufe <marc@schufi.ch>
2018-11-02 08:56:07 +0100

    Add linalg/gleichungssysteme

--- linalg/gleichungssystem.m
+++ linalg/gleichungssystem.m
@@ -1,3 +1,10 @@
-A=[7,-6,2;0,5,-4];
-b=[9;3];
-rref([A b])
+% MATLAB initialisieren:
+clear all; clc; format compact; format short g; 
+
+% Gleichungssystem Lösen
+    % Parameter
+    A=[7,-6,2;0,5,-4]; 
+    % Loesungen
+    b=[9;3];
+    
+    rref([A b])
