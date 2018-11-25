de72c2d856aac33fdbfe097b6633abc9aad7c207
mschaufe <marc@schufi.ch>
2018-11-25 17:51:25 +0100

    Add quicksort

--- informatik/quicksort/main.c
+++ informatik/quicksort/main.c
@@ -23,33 +23,47 @@ void swop(int *p1, int *p2){
 
 }
 
-
 /* Funtion to sort an array */
-void quicksort (int *arr[],int a, int n){
+void quicksort (int arr[],int a, int n){
+    if (n>1){
+        int pivot = arr[n-1];
+        int p = a;
 
-            int pivot = *arr[n - 1];
-            printf("Pivot: %d \n", pivot);
-            int p = 0;
+        for (int i = a; i < n; i++) {
+            if (arr[i] <= pivot && p == i) {
+                p++;
+            } else if (arr[i] <= pivot) {
+                swop(&arr[i], &arr[p]);
+                p++;
 
-            for (int i = a; i < n && n > 0; i++) {
-                if (*arr[i] <= pivot && p == i) {
-                    p++;
-                } else if (*arr[i] <= pivot) {
-                    &
-                    swop(&*arr[i], &*arr[p]);
-                    p++;
-
-                }
             }
+        }
 
+        if (p > a) {
+            int numbers_under_pivot = 0;
+            for (int i = 0; i < p - 1; i++) {
+                numbers_under_pivot++;
+            }
+            //printf("\n- Unter dem Pivot %d sind %d Elemente und der Start ist bei %d\n",pivot, numbers_under_pivot, p-1-numbers_under_pivot);
+            quicksort(arr, p-1-numbers_under_pivot, numbers_under_pivot);
+        }
+
+        if ((n - p) > 1) {
+            int numbers_over_pivot=0;
+            for (int i = 0; i < (n - p); i++) {
+                numbers_over_pivot++;
+            }
+            //printf("\n- Ueber dem Pivot %d sind %d Elemente und der Start ist bei %d\n",pivot, numbers_over_pivot, p);
+            quicksort(arr, p, numbers_over_pivot+p);
+        }
+
+    }
 }
 
 int main() {
-    int arr[] = {9,-3,5,2,6,8,-6,1,3};
+    int arr[] = {5,102,8,-9,3,0,-4,0,23,-6,100,4};
     int n = sizeof(arr)/sizeof(arr[0]);
-    quicksort(&arr,0, n);
+    quicksort(arr,0, n);
     printf("Sorted array: \n");
     printArray(arr, n);
-
-
 }
