5aa31499f9e46b477726396ec9d25193bc63337d
marc <mschaufe>
2020-09-30 08:25:01 +0200

    add folder

---
+++ Bildverarbeitung 2/Serie_01/Principal_Component_Analysis/cmake-build-debug/CMakeFiles/Principal_Component_Analysis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Principal_Component_Analysis.dir/main.cpp.o"
+  "Principal_Component_Analysis"
+  "Principal_Component_Analysis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Principal_Component_Analysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Bildverarbeitung 2/Serie_02/FarbbilderUndFarbraumkonvertierungen/cmake-build-debug/CMakeFiles/FarbbilderUndFarbraumkonvertierungen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FarbbilderUndFarbraumkonvertierungen.dir/main.cpp.o"
+  "FarbbilderUndFarbraumkonvertierungen"
+  "FarbbilderUndFarbraumkonvertierungen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/FarbbilderUndFarbraumkonvertierungen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
